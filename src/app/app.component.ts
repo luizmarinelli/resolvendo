695158251a9b4311112ad305abb08489719ad695
luizmarinelli <luizmarinelli@brq.com>
2020-03-13 02:16:22 -0300

    First commit2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,38 +8,67 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   
   ngOnInit(): void {
-    this.tamanho1 = this.objetoTeste[0].produtos[0].colunas.length;
-    this.tamanho2 = this.objetoTeste[0].produtos.length;
-    console.log("AppComponent -> ngOnInit -> this.tamanho2", this.tamanho2)
-    console.log("AppComponent -> ngOnInit -> this.tamanho1", this.tamanho1)
+    // this.objetoTeste.forEach(objeto => {
+    // console.log("AppComponent -> ngOnInit -> objeto", objeto.periodo)
+    //   objeto.produtos.forEach(produtos => {
+    //     console.log("AppComponent -> ngOnInit -> produtos", produtos.produto);
+    //       produtos.colunas.forEach(colunas => {
+    //         console.log("AppComponent -> ngOnInit -> colunas.nome", colunas.nome);
+    //         console.log("AppComponent -> ngOnInit -> colunas.valor", colunas.valor);
+    //       });
+    //   });
+    // });
   }
   title = 'tabela-do-cao';
 
   tamanho1 = 0;
   tamanho2 = 0;
+
   
   objetoTeste = [
     {
       "periodo" : "Mensal",
-        "produtos" : [
-          {
-            "produto": "Conquista",
-              "colunas" : [
-                { "nome": "Pla. Mensal", "valor": 80 },
-                { "nome": "Meta", "valor": 30 },
-                { "nome": "Meta", "valor": 30 },
-                { "nome": "Ptos. Infor.", "valor": 35 }
-              ]
-          },{
-            "produto": "Conquista2",
-              "colunas" : [
-                { "nome": "Pla. Mensal", "valor": 90 },
-                { "nome": "Meta", "valor": 31 },
-                { "nome": "Meta", "valor": 31 },
-                { "nome": "Ptos. Infor.", "valor": 45 }
-              ]
-            }
-        ] 
+      "produtos" : [
+        {
+          "produto": "Conquista",
+            "colunas" : [
+              { "nome": "Pla. Mensal", "valor": 80 },
+              { "nome": "Meta", "valor": 30 },
+              { "nome": "Meta", "valor": 30 },
+              { "nome": "Ptos. Infor.", "valor": 35 }
+            ]
+        },{
+          "produto": "Conquista2",
+            "colunas" : [
+              { "nome": "Pla. Mensal", "valor": 90 },
+              { "nome": "Meta", "valor": 31 },
+              { "nome": "Meta", "valor": 31 },
+              { "nome": "Ptos. Infor.", "valor": 45 }
+            ]
+          }
+      ] 
+    },
+    {
+      "periodo" : "Trimestral",
+      "produtos" : [
+        {
+          "produto": "Conquista",
+            "colunas" : [
+              { "nome": "Pla. Mensal", "valor": 80 },
+              { "nome": "Meta", "valor": 30 },
+              { "nome": "Meta", "valor": 30 },
+              { "nome": "Ptos. Infor.", "valor": 35 }
+            ]
+        },{
+          "produto": "Conquista2",
+            "colunas" : [
+              { "nome": "Pla. Mensal", "valor": 90 },
+              { "nome": "Meta", "valor": 31 },
+              { "nome": "Meta", "valor": 31 },
+              { "nome": "Ptos. Infor.", "valor": 45 }
+            ]
+          }
+      ] 
     }
   ]
 }
